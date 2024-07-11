c3c4d7bb6344f65ae36beb2aaa8066f41c601ecd
unknown <omarsheikhdieh@gmail.com>
2024-07-11 23:06:53 +0200

    add hbs, api docs template, environment for full-stack

--- server/app/index.ts
+++ server/app/index.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server, update!");
+  res.render('index', { siteTitle: "Hello World" });
 });
 
 app.listen(port, () => {
