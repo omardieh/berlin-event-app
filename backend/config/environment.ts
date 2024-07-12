d311985258eb601fdf781f5f3db77e4154472172
unknown <omarsheikhdieh@gmail.com>
2024-07-13 01:22:06 +0200

    add eslint, fix logger

--- backend/config/environment.ts
+++ backend/config/environment.ts
@@ -9,9 +9,7 @@ dotenv.config({
 });
 
 export const environmentConfig = (app: Express) => {
-  app.use(
-    morgan(":method :url :status :res[content-length] - :response-time ms")
-  );
+  app.use(morgan("dev"));
   app.use(express.static(path.join(__dirname, "../public")));
   app.set("view engine", "hbs");
   app.set("views", path.join(__dirname, "../views"));
